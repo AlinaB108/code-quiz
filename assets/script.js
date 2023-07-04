a1bae9c2478f15a7e53817d75c869f29c57e00cd
Alina Badillo <alinachristabel108@gmail.com>
2023-07-03 23:16:52 -0500

    Added questions and answers in var quizQuestions

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,7 @@
 
 var rules;
 var startBtn;
+var quizField = document.querySelector("quizField");
 var question;
 var answers;
 var choice;
@@ -26,3 +27,66 @@ var initials;
 var submit;
 var backBtn;
 var scoreBtn;
+
+//An array with all quiz questions
+var quizQuestions = [
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: ["msgBox('Hello World');" , "alert('Hello World);" , "msg('Hello World'); , alertBox('Hello World);"],
+    answer: "alert('Hello World);"
+  },
+
+  {
+    question: "How do you call a function named 'myFunction'?",
+    choices: ["call function myFunction()" , "call myFunction()" , "myFunction()" , "function call myFunction()"],
+    answer: "myFunction"
+  },
+
+  {
+    question: "How to write an IF statement?",
+    choices: ["if i === 5 then" , "if (i == 5)" , "if i = 5 then" , "if i = 5"],
+    answer: "if (i == 5)"
+  },
+
+  {
+    question: "How does a FOR loop start?",
+    choices: ["for (i = 0; i <= 5)" , "for i = 1 to 5" , "for (i = 0; i <=5; i++)" , "for (i <=5; i++)"],
+    answer: "for (i = 0; i <=5; i++)"
+  },
+
+  {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onclick" , "onmouseover" , "onmouseclick" , "onchange"],
+    answer: "onclick"
+  },
+
+  {
+    question: "What will the following  code return: Boolean(10 > 9)?",
+    choices: ["NaN", "Undefined", "true", "false"],
+    answer: "true"
+  },
+
+  {
+    question: "Which of the followings are primitive data types in JavaScript?",
+    choices: ["String" , "Number" , "Boolean" , "All of the above"],
+    answer: "All of the above",
+  },
+
+  {
+    question: "What is null in JavaScript?",
+    choices: ["Empty string value" , "Absence of a value" , "Unknown value" , "Zero value"],
+    answer: "Zero value"
+  },
+
+  {
+    question: "Which of the following object represent parameters of current function inside any function?",
+    choices: ["Global" , "arguments" , "this" , "Object"],
+    answer: "arguments"
+  },
+
+  {
+    question: "A variable declared without var keyword inside a function will become _______ variable.",
+    choices: ["local" , "global" , "block" , "undefined"],
+    answer: "global"
+  }
+];
