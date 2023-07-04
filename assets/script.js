866506be17aac937a02a2a0f0b0dc83dcc3775ac
Alina Badillo <alinachristabel108@gmail.com>
2023-07-03 21:23:55 -0500

    Added pseudo code

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// Pseudo code - Code quiz 10 questions and 60seconds to answer
+//Create global variables for timer, score, questions and initials
+// Start the quiz
+  //Add a button to start the quiz, answers should be clickable too
+  //Make a function with a timer and (?)questions and score inside that function
+//Display a question and 4 possible answers
+  //Get a question from the list (probably an array, where a question will be randomly selected from an array)
+//Check if the selected answer is correct (not correct - remove 10sec, if correct - add score)
+  //Display a message about the answered message - Correct!/Wrong!
+  //Display the changes for time on the screen
+//Quiz ends when there is no time left
+//When the game is over need to display the final score and input field for initials
+//Save initials and score (make a list for future score where we can add text with via JS?)
+  //Add a button to save the progress
+
