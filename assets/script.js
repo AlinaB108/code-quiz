6d3918ed6ea9d613d15c141ed585289c3cbae9b8
Alina Badillo <alinachristabel108@gmail.com>
2023-07-05 23:13:21 -0500

    Changed a bit of the structure of index.html, removed some variables from script.js and slightly changed the structure of questions array

--- assets/script.js
+++ assets/script.js
@@ -1,117 +1,131 @@
-// Pseudo code - Code quiz 10 questions and 75 seconds to answer
-//Create global variables for timer, score, questions and initials
-// Start the quiz
-  //Add a button to start the quiz, answers should be clickable too
-  //Make a function with a timer and (?)questions and score inside that function
-//Display a question and 4 possible answers
-  //Get a question from the list (probably an array, where a question will be randomly selected from an array)
-//Check if the selected answer is correct (not correct - remove 10sec, if correct - add score)
-  //Display a message about the answered message - Correct!/Wrong!
-  //Display the changes for time on the screen
-//Quiz ends when there is no time left
-//When the game is over need to display the final score and input field for initials
-//Save initials and score (make a list for future score where we can add text with via JS?)
-  //Add a button to save the progress
+// What is the expected behavior for the user (what user sees)
+//A user opens the page and sees the timer and highscore at the top of the page. Below there is a heading with quiz name and rules, a bit lower there is a start button.
+//When user clicks on a button the quiz starts
+//The timer starts the countdown
+//There is a question and 4 answers, when user clicks on one of the answers he is presented with next question
+//Also, if answer was correct user gets score points, if answer was wrong he loses 15sec
 
-// Why can't I remove an element when it's document.querySelector but I can remove it when it's getElementById?
 var rules = document.getElementById("rules");
 var startBtn = document.getElementById("startBtn");
+var rules = document.getElementById("introduction");
 var quizField = document.getElementById("quizField");
-var question;
-var answers;
-var choice;
-var response;
-var counter = 15;
-var counterDisplay = document.getElementById("counterDisplay");
-var score;
-// Variables for score.html
-var initials;
-var submit;
-var backBtn;
-var scoreBtn;
+var question = document.getElementById("question");
+var answerBtn = document.getElementById("answerBtn");
+var currentQuestionIndex = 0;
+var timeLeft = 75;
+var score = 0;
 
-//An array with all quiz questions
+quizField.style.visibility = "hidden";
+// Create answerBtn and add it later
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz() {
+  startBtn.style.display = "none";
+  introduction.style.display = "none";
+  quizField.style.visibility = "visible";
+  currentQuestionIndex = 0;
+  score = 0;
+  timeLeft = 75;
+}
+
 var quizQuestions = [
   {
     question: "How do you write 'Hello World' in an alert box?",
-    choices: ["msgBox('Hello World');" , "alert('Hello World);" , "msg('Hello World'); , alertBox('Hello World);"],
-    answer: "alert('Hello World);"
+    answers: [
+      {text: "msgBox('Hello World');", correct: false},
+      {text: "alert('Hello World);", correct: true},
+      {text: "msg('Hello World');", correct: false},
+      {text: "alertBox('Hello World);", correct: false}
+    ]
   },
 
   {
     question: "How do you call a function named 'myFunction'?",
-    choices: ["call function myFunction()" , "call myFunction()" , "myFunction()" , "function call myFunction()"],
-    answer: "myFunction"
+    answers: [
+      {text: "call function myFunction()", correct: false},
+      {text: "call myFunction()", correct: false},
+      {text: "myFunction()", correct: true},
+      {text: "function call myFunction()", correct: false}
+    ]
   },
 
   {
     question: "How to write an IF statement?",
-    choices: ["if i === 5 then" , "if (i == 5)" , "if i = 5 then" , "if i = 5"],
-    answer: "if (i == 5)"
+    answers: [
+      {text: "if i === 5 then", correct: false},
+      {text: "if (i == 5)", correct: true},
+      {text: "if i = 5 then", correct: false},
+      {text: "if i = 5", correct: false}
+    ]
   },
 
   {
     question: "How does a FOR loop start?",
-    choices: ["for (i = 0; i <= 5)" , "for i = 1 to 5" , "for (i = 0; i <=5; i++)" , "for (i <=5; i++)"],
-    answer: "for (i = 0; i <=5; i++)"
+    answers: [
+      {text: "for (i = 0; i <= 5)", correct: false},
+      {text: "for i = 1 to 5", correct: false},
+      {text: "for (i = 0; i <=5; i++)", correct: true},
+      {text: "for (i <=5; i++)", correct: false} 
+    ]
   },
 
   {
     question: "Which event occurs when the user clicks on an HTML element?",
-    choices: ["onclick" , "onmouseover" , "onmouseclick" , "onchange"],
-    answer: "onclick"
+    answers: [
+      {text: "onclick", correct: true},
+      {text: "onmouseover", correct: false},
+      {text: "onmouseclick", correct: false},
+      {text: "onchange", correct: false}
+    ]
   },
 
   {
     question: "What will the following  code return: Boolean(10 > 9)?",
-    choices: ["NaN", "Undefined", "true", "false"],
-    answer: "true"
+    answers: [
+      {text: "NaN", correct: false},
+      {text: "Undefined", correct: false},
+      {text: "true", correct: true},
+      {text: "false", correct: false}
+    ]
   },
 
   {
     question: "Which of the followings are primitive data types in JavaScript?",
-    choices: ["String" , "Number" , "Boolean" , "All of the above"],
-    answer: "All of the above",
+    answers: [
+      {text: "String", correct: false},
+      {text: "Number", correct: false},
+      {text: "Boolean", correct: false},
+      {text: "All of the above", correct: true}
+    ]
   },
 
   {
     question: "What is null in JavaScript?",
-    choices: ["Empty string value" , "Absence of a value" , "Unknown value" , "Zero value"],
-    answer: "Zero value"
+    answers: [
+      {text: "Empty string value", correct: false},
+      {text: "Absence of a value", correct: false},
+      {text: "Unknown value", correct: false},
+      {text: "Zero value", correct: true}
+    ]
   },
 
   {
     question: "Which of the following object represent parameters of current function inside any function?",
-    choices: ["Global" , "arguments" , "this" , "Object"],
-    answer: "arguments"
+    answers: [
+      {text: "Global", correct: false},
+      {text: "arguments", correct: true},
+      {text: "this", correct: false},
+      {text: "Object", correct: false}
+    ]
   },
 
   {
     question: "A variable declared without var keyword inside a function will become _______ variable.",
-    choices: ["local" , "global" , "block" , "undefined"],
-    answer: "global"
+    answers: [
+      {text: "local", correct: false},
+      {text: "global", correct: true},
+      {text: "block", correct: false},
+      {text: "undefined", correct: false}
+    ]
   }
 ];
-
-//Adding a button to start the quiz
-startBtn.addEventListener("click", startQuiz);
-//Adding a function to start the quiz when the button is pressed
-function startQuiz () {
-  rules.remove("rules");
-  startBtn.remove("startBtn");
-
-  function countdown() {
-    counter--;
-    document.getElementById("counterDisplay").innerHTML = String(counter);
-    if (counter >= 0) {
-      setTimeout(countdown, 1000);
-      // How not to show -1
-    } else if (counter <= 0) {
-      alert("Time's up!");
-    }
-  };
-  
-  setTimeout(countdown, 1000);
-  // showQuestions()
-};
-
